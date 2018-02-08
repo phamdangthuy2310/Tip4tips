7bdb1e5c24a7e217010522c7f97e6e5575ebd115
Pham Thi Dang Thuy <phamdangthuy2310@gmail.com>
2018-02-08 17:34:20 +0700

    return route lead

--- routes/web/route_lead.php
+++ routes/web/route_lead.php
@@ -18,18 +18,18 @@ Route::post('/leads/ajaxStatus', 'LeadsController@ajaxStatus')->name('leads.ajax
 Route::get('/leads/create', 'LeadsController@create')->name('leads.create');
 Route::post('/leads/create', 'LeadsController@store')->name('leads.store');
 Route::get('/leads/{id}/edit', 'LeadsController@edit')->name('leads.edit');
-Route::post('/leads/update', 'LeadsController@update')->name('leads.update');
+Route::post('/leads/{id}', 'LeadsController@update')->name('leads.update');
 Route::delete('/leads/delete/{id}', 'LeadsController@destroy')->name('leads.destroy');
 Route::get('/leads/{id}', 'LeadsController@show')->name('leads.show');
 Route::get('/leads', 'LeadsController@index')->name('leads.index');
-//Route::resource('leads', 'LeadsController',[
-//    'name' => [
-//        'index' => 'leads.index',
-//        'create' => 'leads.create',
-//        'edit' => 'leads.edit',
-//        'show' => 'leads.show',
-//    ]
-//]);
+Route::resource('leads', 'LeadsController',[
+    'name' => [
+        'index' => 'leads.index',
+        'create' => 'leads.create',
+        'edit' => 'leads.edit',
+        'show' => 'leads.show',
+    ]
+]);
 
 Route::resource('leadsprocesses', 'LeadProcessesController',[
     'name' => [
